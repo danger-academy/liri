e0704ca281f440f302fabf35d124a6f8424b4149
danger-academy <robert.tarkenton@gmail.com>
2019-01-15 15:11:27 -0500

    nearly complete

--- README.md
+++ README.md
@@ -1,4 +1,83 @@
 ## LIRI
 LIRI is a command-line interface (CLI) which accepts properly formatted queries and returns the relevant data.
 
+### Challenges
+Reading through all of the API documentation took a fair amount of time. It also took a few run-throughs to get the npm packages properly installed. 
+***
 
+### APIs used:
+* [Spotify](https://developer.spotify.com/documentation/web-api/)
+* [Bands in Town Events API](https://manager.bandsintown.com/support/bandsintown-api)
+* [OMDb API](http://www.omdbapi.com/)
+
+### Packages used: 
+* [node-spotify-api](https://www.npmjs.com/package/node-spotify-api)
+* [Axios](https://www.npmjs.com/package/axios)
+* [Moment.js](https://www.npmjs.com/package/moment)
+* [fs](https://nodejs.org/api/fs.html)
+* [dotenv](https://www.npmjs.com/package/dotenv)
+
+### Terminal Commands (with gifs to display functionality)
+
+#### concert-this
+`node liri.js concert-this <artist/band>`
+
+![concert-this](assets/images/concert-this.gif)
+
+Uses [Axios](https://www.npmjs.com/package/axios) to retrieve concert data from the [Bands in Town Events API](https://manager.bandsintown.com/support/bandsintown-api). It displays the following information about each show in the terminal:
+
+* Venue name
+* Venue location
+* Show date
+
+This function also uses [Moment.js](https://www.npmjs.com/package/moment) to format the show date (MM//DD/YYYY).
+
+If no band is provided, it will display results for Murder by Death.
+
+#### spotify-this-song
+`node liri.js spotify-this-song <song name>`
+
+![spotify-this-song](assets/images/spotify-this-song.gif)
+
+Uses the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) to get song data from Spotify and displays the following information:
+
+* Artist(s)
+* The song's name
+* A preview link of the song from Spotify
+* The album that the song is from
+
+If no song name is provided, it will display the results for "Rumbrave" by Murder by Death.
+
+#### movie-this
+`node liri.js movie-this <movie name>`
+
+![movie-this](assets/images/movie-this.gif)
+
+Uses [Axios](https://www.npmjs.com/package/axios) to get movie data from the [OMDb API](http://www.omdbapi.com/) and outputs the movie's data to the terminal:
+
+* Title
+* Year produced
+* IMDB Rating
+* Rotten Tomatoes Rating
+* Country where produced
+* Language
+* Plot
+* Cast
+
+If no movie name is provided, it will display the results for "Murder by Death".
+
+#### do-what-it-says
+`node liri.js do-what-it-says <filename>`
+
+![do-what-it-says](assets/images/do-what-it-says.gif)
+
+Uses Node's [fs](https://nodejs.org/api/fs.html) package to read a file with a command structured as `spotify-this-song,"I Shot An Arrow"`, then will run the command as if entered via the terminal. It will accept the filename as a parameter, but if none is provided then will pull from random.txt.
+***
+
+### Securing API keys
+Uses [dotenv](https://www.npmjs.com/package/dotenv) to add a layer of data security, by storing the relevant api keys in a separate file 
+
+### log.txt
+As a bonus feature, uses [fs](https://nodejs.org/api/fs.html) to append the output (including command and search parameter) to a log file via `logToFile()`, like so:
+
+![log](assets/images/log.png)
---
+++ liri.js
@@ -0,0 +1,177 @@
+// Require modules
+require("dotenv").config();
+const Spotify = require("node-spotify-api");
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs");
+
+// Retrieve API keys from .env
+const keys = require("./keys.js");
+const spotify = new Spotify(keys.spotify);
+const bandsInTownID = keys.bandsInTown.id;
+const omdbID = keys.omdb.id;
+
+// Command to process arguments
+const command = process.argv[2];
+
+// Use slice and join to properly format queries for api calls
+const query = process.argv.slice(3).join(" ");
+
+// Command class
+class Command {
+    constructor(run) {
+        this.run = run;
+    }
+}
+
+// Define command0
+const command0 = {
+    "spotify-this-song": new Command(callSongData),
+    "movie-this": new Command(callMovieData),
+    "concert-this": new Command(callConcertData),
+    "do-what-it-says": new Command(doWhatItSays)
+}
+
+    // Run command using the query
+    if (command0[command]) {
+        command0[command].run(query);
+    }
+    else {
+        printUsage();
+    }
+
+// Use node-spotify-api to call song data
+function callSongData(query) {
+    if (!query) {
+        query = "Rumbrave Murder by Death";
+    }
+    spotify.search({ type: 'track', query: query, limit: 1 }).then(response => {
+        if (response.tracks.items.length < 1) {
+            throw "I kin no find yer tune. Wid ya like t' try a'nuther?";
+        }
+        else {
+            const artistName = response.tracks.items[0].artists[0].name;
+            const query = response.tracks.items[0].name;
+            const link = response.tracks.items[0].external_urls.spotify;
+            const albumName = response.tracks.items[0].album.name;
+
+            let output = "Artist: " + artistName;
+            output += "\nSong Name: " + query;
+            output += "\nLink: " + link;
+            output += "\nAlbum: " + albumName;
+
+            // Send to console and to log.txt                
+            console.log(output);
+            logToFile("spotify-this-song", query, output);
+        }
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+// Use axios to call concert data from bandsintown
+function callConcertData(query) {
+    if (!query) {
+        query = "Murder by Death";
+    }
+    axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=" + bandsInTownID).then(response => {
+        if (!Array.isArray(response.data) || response.data.length < 1) {
+            throw "I kin no find yer show. Wid ya like t' try a'nuther?";
+        }
+        else {
+            let output = "";
+            response.data.forEach(concert => {
+                output += concert.venue.name;
+                output += "\n" + concert.venue.city + ", " + concert.venue.region + " " + concert.venue.country;
+                output += "\n" + moment(concert.datetime.substr(0, 10), "YYYY-MM-DD").format("MM/DD/YYYY") + "\n\n";
+            });
+            output = output.trim();
+            // Send to console and log.txt
+            console.log(output)
+            logToFile("concert-this", query, output)
+        }
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+// Use axios to call movie data from omdb
+function callMovieData(query) {
+    if (!query) {
+        query = "Murder by Death";
+    }
+    axios.get("http://www.omdbapi.com/?t=" + query + "&plot=short&apikey=" + omdbID).then(response => {
+        if (response.data.Response === 'False') {
+            throw "I kin find nah such film. Wid ya like t' try a'nuther?";
+        }
+        else {
+            let output = "Title: " + response.data.Title;
+            output += "\nReleased: " + response.data.Year;
+            output += "\nIMDB rating: " + response.data.imdbRating;
+            const rottenRating = response.data.Ratings.filter(rating => rating.Source.includes("Rotten Tomatoes"))[0];
+            if (rottenRating) {
+                output += "\nRotten Tomatoes Rating: " + rottenRating.Value;
+            }
+            else {
+                output += "\nRotten Tomatoes Rating: N/A";
+            }
+            output += "\nLanguage: " + response.data.Language;
+            output += "\nPlot: " + response.data.Plot;
+            output += "\nActors: " + response.data.Actors;
+
+            // Send to console and log.txt
+            console.log(output);
+            logToFile("movie-this", query, output);
+        }
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+// Call from random.txt
+function doWhatItSays(fileName) {
+    if (!fileName) {
+        fileName = "random.txt";
+    }
+    try {
+        fs.readFile(fileName, "utf8", (error, data) => {
+            if (error) throw error;
+            const dataArr = data.split(",");
+            const command = dataArr[0];
+            const query = dataArr[1];
+            // This command will loop forever and ever?
+            if (command !== "do-what-it-says") { 
+                fs.appendFile("log.txt", "do-what-it-says " + fileName + "\n", error => {
+                    if (error) throw error;
+                });
+                command0[command].run(query);
+            }
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+// Append command and result to log.txt
+function logToFile(command, searchTerm, result) {
+    // Format content to append by adding command, searchTerm, result, and divider
+    const output = command + " " + searchTerm + "\n" + result + "\n\n" + new Array(75).join("=") + "\n\n";
+    try {
+        fs.appendFile("log.txt", output, error => {
+            if (error) throw error;
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+// Print command0 instructions to console
+function printUsage() {
+    console.log("USAGE:");
+    console.log("node liri.js concert-this <artist/band name here>");
+    console.log("node liri.js spotify-this-song <song name here>");
+    console.log("node liri.js movie-this <movie name here>");
+    console.log("node liri.js do-what-it-says <filename>");
+}
